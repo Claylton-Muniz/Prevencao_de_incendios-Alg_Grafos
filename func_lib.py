d5d0b500518ae05b0351538c2ddcc72b634e4d27
Claylton-Muniz <claymuniz67@gmail.com>
2025-05-15 13:07:07 -0300

    Otimização do código para deixar mais legivel e adição da lógica de equipes não estarem no mesmo lugar ao mesmo tempo

--- func_lib.py
+++ func_lib.py
@@ -1,105 +1,72 @@
-import heapq
-from networkx import draw, draw_networkx_edge_labels, spring_layout, get_edge_attributes
-import matplotlib.pyplot as plt
 import os
-import shutil
-
 import heapq
+import shutil
+import matplotlib.pyplot as plt
 
-def dijkstra_caminho(G, origem, destino):
-    c = {node: float('inf') for node in G} # c[i] = infinito
-    prev = {node: None for node in G} # guarda nó anterior para construir o caminho
-    c[origem] = 0
-    heap = [(0, origem)]
-
-    while heap:
-        current_c, u = heapq.heappop(heap)
-
-        if u == destino:
-            break
-
-        for vizinho, attrs in G[u].items():  # attrs contém as propriedades da aresta
-            weight = attrs.get('weight', 1)  # Pega o peso da aresta, com valor padrão de 1
-            aux = current_c + weight
-            if aux < c[vizinho]:
-                c[vizinho] = aux
-                prev[vizinho] = u
-                heapq.heappush(heap, (aux, vizinho))
-
-    # Reconstruir o caminho
-    caminho = []
-    u = destino
-    while u is not None:
-        caminho.append(u)
-        u = prev[u]
-    caminho.reverse()
-
-    if caminho and caminho[0] == origem:
-        return caminho
-    else:
-        return []  # Caminho não encontrado
-
-
-# modificado para fazer a largura de 1 só vertice
-def bfs_mod(G, m, vert, alastramento, prioridade, postos, equipes, fogo):
-    # python não tem ponteiros então isso está substituindo o u ← G[w].prox e o u ← u.prox
-    for u in G[vert]:
-        if m[u] == "verde":
-            if m[u] != "azul" and m[u] != "amarelo":
-                m[u] = "vermelho"
-                fogo.append(u)
-                heapq.heappush(alastramento, (prioridade + 1, u))
-                
-    # Removendo os elementos comuns
-    fogo = [x for x in fogo if m[x] not in ('amarelo', 'azul')]
-    print(f"eq: {equipes} \nm: {m} \nf: {fogo}")
-
-    return m
+from func_graf import bfs_mod, dijkstra_caminho
+from networkx import draw, draw_networkx_edge_labels, spring_layout, get_edge_attributes
 
 
 def criar_grafo(G, dados):
-    for i in range(dados["num_vertices"]):
-        G.add_node(f"V{i}")
-
-    for aresta in dados["arestas"]:
-        G.add_edge(f"V{aresta[0]}", f"V{aresta[2]}", weight=aresta[1])
+    G.add_nodes_from(f"V{i}" for i in range(dados["num_vertices"]))
+    for v1, peso, v2 in dados["arestas"]:
+        G.add_edge(f"V{v1}", f"V{v2}", weight=peso)
 
 
 def criar_imagem(G, pos, cores, edge_labels, pasta_imgs, passo, atual, alastramento):
     plt.figure(figsize=(8, 6))
 
-    if passo == 0:
-        plt.title(f"Passo {passo}: Fogo iniciou em {atual}")
-    else:
-        plt.title(
-            f"Passo {passo}: Fogo se alastrou por {atual}"
-            + "".join(f", {v[1]}" for v in alastramento)
-        )
+    titulo = (
+        f"Passo {passo}: Fogo iniciou em {atual}"
+        if passo == 0 else
+        f"Passo {passo}: Fogo se alastrou por {atual}" +
+        "".join(f", {v[1]}" for v in alastramento)
+    )
+    
+    plt.title(titulo)
 
     draw(G, pos, with_labels=True, node_color=cores)
     draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color="red")
 
-    caminho_imagem = os.path.join(pasta_imgs, "slide.png")
-
-    plt.savefig(caminho_imagem)
+    plt.savefig(os.path.join(pasta_imgs, "slide.png"))
+    plt.savefig(os.path.join(pasta_imgs, "slide-{}.png".format(passo)))
     plt.close()
 
 
-def mostrar_grafo(G, dados, postos, coleta_agua, equipe):
-    pasta_imgs = "imgs"
+def rederinzar_imagem(
+    G, m, pos, edge_labels, pasta_imgs, postos, equipe, coleta_agua, atual, passo, alastramento
+    ):
+    # Define cores dos nós
+    cores = [
+        "#3F48CC" if vert in postos and vert in equipe else 
+        "blue" if vert in postos else 
+        "orange" if vert in equipe else 
+        "yellow" if vert in coleta_agua and m[vert] == "vermelho" else 
+        "red" if atual == "vermelho" or m[vert] == "vermelho" else 
+        "lightblue" if vert in coleta_agua else 
+        "lightgreen"
+        for vert in G.nodes()
+    ]
+    
+    # Cria a imagem do passo atual
+    criar_imagem(
+        G, pos, cores, edge_labels, pasta_imgs, passo, atual, alastramento
+    )
+    
+    input("Enter...")
+    
 
+def mostrar_grafo(G, dados, postos, coleta_agua, equipe):
+    
     # Cria a pasta se não existir, ou limpa se já existir
+    pasta_imgs = "imgs"
     if os.path.exists(pasta_imgs):
         shutil.rmtree(pasta_imgs)
     os.makedirs(pasta_imgs)
     
-    fogo = []
+    # Define a origem do fogo
     origem = f"V{dados['fogo']}"
-    fogo.append(origem)
-
-    # Layout e pesos das arestas
-    pos = spring_layout(G, seed=42)
-    edge_labels = get_edge_attributes(G, "weight")
+    fogo = [origem]
 
     # Declaração inicial das marcas na BFS.
     # Alterei para verde e vermelho para fazer sentido com o fogo
@@ -110,97 +77,69 @@ def mostrar_grafo(G, dados, postos, coleta_agua, equipe):
     diminuir a complexidade e não perder 
     as marcas nas iterações
     """
-
-    passo = 0
+    
+    # Layout e pesos das arestas
+    pos = spring_layout(G, seed=42)
+    edge_labels = get_edge_attributes(G, "weight")
 
     alastramento = []
     heapq.heappush(alastramento, (0, origem))
     
-    # Criar uma função para diminuir isso aqui ----------------------------------
-    
-    # Define cores dos nós
-    cores = [
-        "#3F48CC"
-        if vert in postos and vert in equipe
-        else "blue"
-        if vert in postos
-        else "orange"
-        if vert in equipe
-        else "yellow"
-        if vert in coleta_agua and m[vert] == "vermelho"
-        else "red"
-        if origem == "vermelho" or m[vert] == "vermelho"
-        else "lightblue"
-        if vert in coleta_agua
-        else "lightgreen"
-        for vert in G.nodes()
-    ]
-
-    # Cria a imagem do passo atual
-    criar_imagem(
-        G, pos, cores, edge_labels, pasta_imgs, passo, origem, alastramento
+    passo = 0
+    rederinzar_imagem(
+        G, m, pos, edge_labels, pasta_imgs, postos, equipe, coleta_agua, origem, passo, alastramento
     )
-    
-    input("Enter...")
 
     prioridade, atual = heapq.heappop(alastramento)
-    print(m[atual])
-    input("test...")
     
     while fogo:
         if m[atual] != "vermelho":
             prioridade, atual = heapq.heappop(alastramento)  # remove da heap
         
         for i, v in enumerate(equipe):
-            tam = 0
+            tam = float("inf")
+            
+            # u anda nos vizinhos do fogo ou no fogo
             for u in list(G[atual]) + [atual]:
                 if v == u:
                     continue
                 caminho = dijkstra_caminho(G, v, u)
-                # print(f"Menor caminho de {v} até {u}: {caminho} -> tam: {len(caminho)}")
-                if len(caminho) < tam and u not in postos:
+                print(f"{atual} - Menor caminho de {v} até {u}: {caminho} -> tam: {len(caminho)}")
+                if (len(caminho) < tam and 
+                    u not in postos and
+                    caminho[1] not in equipe):
                     equipe[i] = caminho[1]
                     tam = len(caminho)
-                else:
-                    equipe[i] = caminho[1]
-                    tam = len(caminho)
+                
+            # texto para diexar bonito, não afeta a lógica    
             acao = "apagando o fogo" if m[equipe[i]] == "vermelho" else "evitando que o fogo se alastrasse"
             if equipe[i] in list(G[atual]) + [atual]:
-                print(f"----> A equipe - {v}, se moveu para {equipe[i]} {acao}")
+                print(f"----> A equipe que estava em {v}, se moveu para {equipe[i]} {acao}")
                 m[equipe[i]] = "amarelo"
             else:
                 print(f"----> A equipe - {v}, se moveu para {equipe[i]}")
         
         bfs_mod(G, m, atual, alastramento, prioridade, postos, equipe, fogo)
-
-        # Define cores dos nós
-        cores = [
-            "#3F48CC"
-            if vert in postos and vert in equipe
-            else "blue"
-            if vert in postos
-            else "orange"
-            if vert in equipe
-            else "yellow"
-            if vert in coleta_agua and m[vert] == "vermelho"
-            else "red"
-            if atual == "vermelho" or m[vert] == "vermelho"
-            else "lightblue"
-            if vert in coleta_agua
-            else "lightgreen"
-            for vert in G.nodes()
-        ]
-
-        # Cria a imagem do passo atual
-        criar_imagem(
-            G, pos, cores, edge_labels, pasta_imgs, passo, atual, alastramento
-        )
+        
+        # Removendo os elementos comuns
+        fogo = [x for x in fogo if m[x] not in ('amarelo', 'azul')]
 
         passo += 1
-        input("Enter...")
+        
+        rederinzar_imagem(
+            G, m, pos, edge_labels, pasta_imgs, postos, equipe, coleta_agua, atual, passo, alastramento
+        )
+        
+        print(f"fogo: {fogo}")
+        if not fogo:
+            break
 
         print(f"equipes: {equipe}")
         
         if not alastramento:
+            # Define cores dos nós
+            rederinzar_imagem(
+                G, m, pos, edge_labels, pasta_imgs, postos, equipe, coleta_agua, atual, passo, alastramento
+            )
+            
             print("O fogo foi completamente controlado.")
-            break
